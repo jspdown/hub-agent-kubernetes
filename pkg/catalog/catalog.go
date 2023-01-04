c40b7684035677c8d05829a9e60624a28b77d485
Harold Ozouf <harold.ozouf@gmail.com>
2023-01-04 15:51:00 +0100

    feat: catalog syncing

---
+++ pkg/catalog/catalog.go
@@ -0,0 +1,83 @@
+/*
+Copyright (C) 2022-2023 Traefik Labs
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package catalog
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	hubv1alpha1 "github.com/traefik/hub-agent-kubernetes/pkg/crd/api/hub/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// Catalog is a catalog of services exposed through a unified API.
+type Catalog struct {
+	WorkspaceID string `json:"workspaceId"`
+	ClusterID   string `json:"clusterId"`
+	Name        string `json:"name"`
+
+	Version string `json:"version"`
+
+	Services      []Service `json:"services,omitempty"`
+	CustomDomains []string  `json:"customDomains,omitempty"`
+
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+}
+
+// CustomDomain holds domain information.
+type CustomDomain struct {
+	Name     string `json:"name"`
+	Verified bool   `json:"verified"`
+}
+
+// Service is a service within a catalog.
+type Service = hubv1alpha1.CatalogService
+
+// Resource builds the v1alpha1 EdgeIngress resource.
+func (e *Catalog) Resource() (*hubv1alpha1.Catalog, error) {
+	spec := hubv1alpha1.CatalogSpec{
+		Services:      e.Services,
+		CustomDomains: e.CustomDomains,
+	}
+
+	specHash, err := spec.Hash()
+	if err != nil {
+		return nil, fmt.Errorf("compute spec hash: %w", err)
+	}
+
+	var urls []string
+	var verifiedCustomDomains []string
+	for _, domain := range e.CustomDomains {
+		urls = append(urls, "https://"+domain)
+		verifiedCustomDomains = append(verifiedCustomDomains, domain)
+	}
+
+	return &hubv1alpha1.Catalog{
+		ObjectMeta: metav1.ObjectMeta{Name: e.Name},
+		Spec:       spec,
+		Status: hubv1alpha1.CatalogStatus{
+			Version:       e.Version,
+			SyncedAt:      metav1.Now(),
+			CustomDomains: verifiedCustomDomains,
+			URLs:          strings.Join(urls, ","),
+			SpecHash:      specHash,
+		},
+	}, nil
+}
